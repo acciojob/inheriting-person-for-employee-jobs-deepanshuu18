5983f6b2c81d2e5f9db123e4f28a577422ddd3f0
DEEPANSHU <deepanshufea07@gmail.com>
2024-12-23 13:28:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
+/*  your css code here. If applicable */
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	this.name=name;
+	this.age=age;
+}
+greet() {console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  }
 
-function Employee(name, age, jobTitle) {}
+function Employee(name, age, jobTitle) {
+	constructor( name,age,jobTitle){
+		super(name,age);
+		this.jobTitle=jobTitle;
+	}
+}
 
+ jobGreet() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+  }
+}
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
