3b3d05fdbb866fe692dbaa7a4a0d800fedd47ba0
DEEPANSHU <deepanshufea07@gmail.com>
2024-12-23 13:36:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -19,7 +19,9 @@ Employee.prototype.jobGreet = function() {
   console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 };
 
+const person = new Person("Alice", 25);
+person.greet();  // Hello, my name is Alice, I am 25 years old.
 
-// Do not change code below this line
-window.Person = Person;
-window.Employee = Employee;
+const employee = new Employee("Bob", 30, "Manager");
+employee.jobGreet();  // Hello, my name is Bob, I am 30 years old, and my job title is Manager.
+employee.greet();     // Hello, my name is Bob, I am 30 years old. (inherited from Person)
